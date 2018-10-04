17e4f44b3e6d261a7964028e1f9d0c8a565578f7
wailunlim <wailun@u.nus.edu>
2018-10-05 01:52:42 +0800

    Optimise imports for certain files

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -23,8 +23,8 @@ import seedu.address.model.Model;
 import seedu.address.model.contact.Address;
 import seedu.address.model.contact.Email;
 import seedu.address.model.contact.Name;
-import seedu.address.model.person.Person;
 import seedu.address.model.contact.Phone;
+import seedu.address.model.person.Person;
 import seedu.address.model.tag.Tag;
 
 /**
--- src/main/java/seedu/address/storage/XmlAdaptedPerson.java
+++ src/main/java/seedu/address/storage/XmlAdaptedPerson.java
@@ -13,8 +13,8 @@ import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.contact.Address;
 import seedu.address.model.contact.Email;
 import seedu.address.model.contact.Name;
-import seedu.address.model.person.Person;
 import seedu.address.model.contact.Phone;
+import seedu.address.model.person.Person;
 import seedu.address.model.tag.Tag;
 
 /**
