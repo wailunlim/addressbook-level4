d7d325232ae26b5f631d9cdf4e8411a667b65015
dongsiji <dongsiji96@gmail.com>
2018-10-11 21:52:27 +0800

    Update CommandBoxTest.java

--- src/test/java/seedu/address/ui/CommandBoxTest.java
+++ src/test/java/seedu/address/ui/CommandBoxTest.java
@@ -9,15 +9,15 @@ import org.junit.Test;
 
 import guitests.guihandles.CommandBoxHandle;
 import javafx.scene.input.KeyCode;
-import seedu.address.commons.core.Messages;
 import seedu.address.logic.Logic;
 import seedu.address.logic.LogicManager;
+import seedu.address.logic.commands.FindCommand;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 
 public class CommandBoxTest extends GuiUnitTest {
 
-    private static final String COMMAND_THAT_SUCCEEDS = Messages.MESSAGE_LIST_ALL_PERSON;
+    private static final String COMMAND_THAT_SUCCEEDS = FindCommand.COMMAND_WORD;
     private static final String COMMAND_THAT_FAILS = "invalid command";
 
     private ArrayList<String> defaultStyleOfCommandBox;
