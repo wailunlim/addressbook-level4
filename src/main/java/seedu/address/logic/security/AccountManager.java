7caf17ba896dba5f30a3d2a021599d56fe226e00
Gan Chin Yao <>
2018-10-14 14:25:41 +0800

    Add loginWithAccount method

--- src/main/java/seedu/address/logic/security/AccountManager.java
+++ src/main/java/seedu/address/logic/security/AccountManager.java
@@ -33,7 +33,7 @@ public class AccountManager {
     /**
      * After user has successfully logged in, set loginSuccess field to true.
      */
-    public void setLoginSuccess() {
+    private void setLoginSuccess() {
         loginSuccess = true;
     }
 
@@ -43,5 +43,18 @@ public class AccountManager {
      */
     public static boolean isLoginSuccess() {
         return loginSuccess;
+    }
+
+    /**
+     * User attempts to login with his account.
+     * @param account The account to login with
+     * @return true if login is successful, false otherwise.
+     */
+    public boolean loginWithAccount(Account account) {
+        if (accountList.hasAccount(account)) {
+            setLoginSuccess();
+        }
+
+        return isLoginSuccess();
     }
 }
