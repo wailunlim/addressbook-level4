d4437cb8594ba464a224f1f22e29ef365f669bb6
wailunlim <wailun@u.nus.edu>
2018-10-17 13:37:30 +0800

    Add class to parse list serviceprovider command

---
+++ src/main/java/seedu/address/logic/parser/ListServiceProviderCommandParser.java
@@ -0,0 +1,22 @@
+package seedu.address.logic.parser;
+
+import java.util.function.Predicate;
+
+import seedu.address.logic.commands.ListCommand;
+import seedu.address.model.contact.Contact;
+import seedu.address.model.contact.EntryContainsKeywordsPredicate;
+import seedu.address.model.serviceprovider.ServiceProvider;
+
+/**
+ * Parses input arguments and creates a new ListCommand object
+ */
+public class ListServiceProviderCommandParser extends ListCommandParser {
+    public static final Predicate<Contact> CONTACT_FILTER_SERVICE_PROVIDER =
+            contact -> contact instanceof ServiceProvider;
+
+    @Override
+    public ListCommand parse(String args) {
+        return new ListCommand(new EntryContainsKeywordsPredicate(getListOfNameKeywords(args)),
+                CONTACT_FILTER_SERVICE_PROVIDER);
+    }
+}
