13bc6689f3bc24c27f920dc97a5886564681157c
liaujianjie <liaujianjie@gmail.com>
2018-10-18 22:42:52 +0800

    Minor refactoring and throw when trying searching for entities of invalid id

--- src/main/java/seedu/address/logic/parser/MatchmakeCommandParser.java
+++ src/main/java/seedu/address/logic/parser/MatchmakeCommandParser.java
@@ -1,35 +1,31 @@
 package seedu.address.logic.parser;
 
-import java.util.Arrays;
-import java.util.Collections;
-
-import seedu.address.logic.commands.MatchmakeCommand;
-import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.logic.commands.MatchMakeCommand;
 
 /**
- * Parses input arguments and creates a new MatchmakeCommand object
+ * Parses input arguments and creates a new MatchMakeCommand object
  */
-public class MatchmakeCommandParser implements Parser<MatchmakeCommand> {
+public class MatchmakeCommandParser implements Parser<MatchMakeCommand> {
 
     private final int ENTITY_TYPE = 0;
     private final int ENTITY_ID = 1;
 
     /**
-     * Parses the given {@code String} of arguments in the context of the MatchmakeCommand
-     * and returns an MatchmakeCommand object for execution.
+     * Parses the given {@code String} of arguments in the context of the MatchMakeCommand
+     * and returns an MatchMakeCommand object for execution.
      */
-    public MatchmakeCommand parse(String args) throws ParseException {
+    public MatchMakeCommand parse(String args) {
         String trimmedArgs = args.trim();
 
         if (trimmedArgs.isEmpty()) {
-            return new MatchmakeCommand(null, null);
+            return new MatchMakeCommand(null, null);
         }
 
         String[] entity = trimmedArgs.split("#");
         String entityType = entity[ENTITY_TYPE];
         String entityId = entity[ENTITY_ID];
 
-        return new MatchmakeCommand(entityType, entityId);
+        return new MatchMakeCommand(entityType, entityId);
     }
 
 }
