84509190c2a52b49e8a4a136f255d44810c1697c
NightYeti <chinyao95@gmail.com>
2018-10-19 21:10:54 +0800

    Add junit test for XmlAccountStorageTest class

--- src/test/java/seedu/address/storage/XmlAccountStorageTest.java
+++ src/test/java/seedu/address/storage/XmlAccountStorageTest.java
@@ -1,7 +1,10 @@
 package seedu.address.storage;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -12,13 +15,17 @@ import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 
 import seedu.address.commons.exceptions.DataConversionException;
+import seedu.address.commons.util.FileUtil;
 import seedu.address.model.account.AccountList;
+import seedu.address.testutil.TypicalAccount;
 
 
 public class XmlAccountStorageTest {
 
     private static final String INVALID_FILEPATH = "someInvalidPath.xml";
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "XmlAccountStorageTest");
+    private static final Path TEST_ACCOUNTLIST_PATH = Paths.get("src", "test", "data",
+            "XmlAccountStorageTest", "accountlist.xml");
 
     @Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
@@ -63,5 +70,27 @@ public class XmlAccountStorageTest {
     public void getAccountList_invalidAndValidUsername_throwDataConversionException() throws Exception {
         thrown.expect(DataConversionException.class);
         getAccountList("InvalidAndValidUsernameAccountList copy.xml");
+    }
+
+    @Test
+    public void populateRootAccountIfMissing_populateForFirstTime() {
+        try {
+            Files.deleteIfExists(TEST_ACCOUNTLIST_PATH);
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+        assertFalse(FileUtil.isFileExists(TEST_ACCOUNTLIST_PATH));
+        AccountStorage accountStorage = new XmlAccountStorage(TEST_ACCOUNTLIST_PATH);
+        accountStorage.populateRootAccountIfMissing(TypicalAccount.ROOTACCOUNT);
+        assertTrue(FileUtil.isFileExists(TEST_ACCOUNTLIST_PATH));
+
+        // asserts that the file contains the default account.
+        try {
+            AccountList accountList = accountStorage.getAccountList(TEST_ACCOUNTLIST_PATH);
+            assertTrue(accountList.getList().size() == 1);
+            assertTrue(accountList.getList().get(0).equals(TypicalAccount.ROOTACCOUNT));
+        } catch (IOException | DataConversionException e) {
+            throw new RuntimeException();
+        }
     }
 }
