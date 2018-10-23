18ea3eb817b208b50c6d32d7b71cc690fdbe55d7
dongsiji <dongsiji96@gmail.com>
2018-10-24 02:41:49 +0800

    Fix checkstyle errors

--- src/main/java/seedu/address/logic/parser/ListCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ListCommandParser.java
@@ -7,9 +7,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import java.util.stream.Stream;
 
 import seedu.address.logic.commands.ListCommand;
