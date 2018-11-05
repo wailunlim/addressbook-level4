edbad5c20e4cec4f349281251fdaee018a341537
NightYeti <chinyao95@gmail.com>
2018-11-05 13:36:07 +0800

    Add a message to feedback to user on 'register account' before logging in

--- src/main/java/seedu/address/logic/commands/RegisterAccountCommand.java
+++ src/main/java/seedu/address/logic/commands/RegisterAccountCommand.java
@@ -39,6 +39,8 @@ public class RegisterAccountCommand extends Command {
             + "Please make sure to use only \"r/superuser\" "
             + "or r/readonlyuser\" for role.";
     public static final String MESSAGE_FAILURE_FILENOTFOUND = "Failed to find the file to save account to.";
+    public static final String MESSAGE_REGISTERACCOUNT_INVOKEATLOGIN = "You can only register an account after "
+            + "logging in. Please contact your admin to get an account.";
     public static final String MESSAGE_FAILURE_DUPLICATE = "Username is taken. Please try again with another username.";
     public static final String MESSAGE_INVALIDROLE = "Role should contains only"
             + "\"superuser\" or \"readonlyuser\".";
