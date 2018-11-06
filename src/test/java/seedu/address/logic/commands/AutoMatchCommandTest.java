af4120b65bb8e1ae053a211e039037f1f90818fd
liaujianjie <liaujianjie@gmail.com>
2018-11-06 21:23:49 +0800

    Add tests for AutoMatchCommand#equals

--- src/test/java/seedu/address/logic/commands/AutoMatchCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AutoMatchCommandTest.java
@@ -1,10 +1,14 @@
 package seedu.address.logic.commands;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.ContactType;
 
 public class AutoMatchCommandTest {
 
@@ -33,5 +37,36 @@ public class AutoMatchCommandTest {
     public void constructor_invalidContactType_throwsParseException() throws ParseException {
         thrown.expect(ParseException.class);
         new AutoMatchCommand("dog", "1");
+    }
+
+    @Test
+    public void equals_exactAutoMatchCommands_shouldBeEqual() throws ParseException {
+        ExpectedException.none();
+        AutoMatchCommand command = new AutoMatchCommand(ContactType.CLIENT.toString(), "1");
+        assertEquals(command, command);
+    }
+
+    @Test
+    public void equals_sameAutoMatchCommands_shouldBeEqual() throws ParseException {
+        ExpectedException.none();
+        AutoMatchCommand commandA = new AutoMatchCommand(ContactType.CLIENT.toString(), "1");
+        AutoMatchCommand commandB = new AutoMatchCommand(ContactType.CLIENT.toString(), "1");
+        assertEquals(commandA, commandB);
+    }
+
+    @Test
+    public void equals_differentTypeAutoMatchCommands_shouldNotBeEqual() throws ParseException {
+        ExpectedException.none();
+        AutoMatchCommand commandA = new AutoMatchCommand(ContactType.CLIENT.toString(), "1");
+        AutoMatchCommand commandB = new AutoMatchCommand(ContactType.VENDOR.toString(), "1");
+        assertNotEquals(commandA, commandB);
+    }
+
+    @Test
+    public void equals_differentIdAutoMatchCommands_shouldNotBeEqual() throws ParseException {
+        ExpectedException.none();
+        AutoMatchCommand commandA = new AutoMatchCommand(ContactType.CLIENT.toString(), "1");
+        AutoMatchCommand commandB = new AutoMatchCommand(ContactType.CLIENT.toString(), "2");
+        assertNotEquals(commandA, commandB);
     }
 }
