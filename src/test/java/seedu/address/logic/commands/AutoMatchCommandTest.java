1daf4ace6db2e9e4acafbb6a16ec732cf2a3d7cb
liaujianjie <liaujianjie@gmail.com>
2018-11-06 21:11:54 +0800

    Add tests for ContactType#fromString

--- src/test/java/seedu/address/logic/commands/AutoMatchCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AutoMatchCommandTest.java
@@ -4,12 +4,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
-import seedu.address.logic.CommandHistory;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 public class AutoMatchCommandTest {
-
-    private static final CommandHistory EMPTY_COMMAND_HISTORY = new CommandHistory();
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
