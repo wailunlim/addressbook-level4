7d304173621412bf7f7bcba1f96f58c9453b19b0
wailunlim <wailun@u.nus.edu>
2018-11-10 11:44:04 +0800

    Add no write privilege add test

--- src/test/java/seedu/address/testutil/TypicalAccount.java
+++ src/test/java/seedu/address/testutil/TypicalAccount.java
@@ -14,8 +14,10 @@ public class TypicalAccount {
     public static final Account SIMUN = new Account("simun", "@myPassword", Role.SUPER_USER);
     public static final Account ROSE = new Account("whiterose", "@myPassword", Role.READ_ONLY_USER);
     public static final Account LUNA = new Account("alex95", "_MyPassword", Role.READ_ONLY_USER);
+    public static final Account READ_ONLY_USER_ACCOUNT = new Account("readonlyuser", "@myPassword",
+            Role.READ_ONLY_USER);
     public static final Account ROOTACCOUNT = new Account("rootUser", "rootPassword", Role.SUPER_USER);
-    public static final Account EMPTYUSERNAME_ACCOUNT = new Account("", "rootPassword", Role.SUPER_USER);
+    public static final Account EMPTY_USERNAME_ACCOUNT = new Account("", "rootPassword", Role.SUPER_USER);
 
     /**
      * Returns an {@code Account} with all the typical Accounts.
